6e079a30e1e9fbc9c846f37bf0254c0c57f5b296
Kai <kai5266@gmail.com>
2021-02-05 15:10:57 +0800

    branch test

--- app/src/main/java/com/example/shipmentsystem/MainActivity.kt
+++ app/src/main/java/com/example/shipmentsystem/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
             add(R.id.main_Layout, itemFragment , itemFragment ::class.java.name)
             commit()
         }
-        val a = ""
-
     }
+    val git = "test"
+
 }
