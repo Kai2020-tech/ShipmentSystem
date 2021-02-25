6200a77f64a260906332e36217cf90d73cc9dba3
Kai2020-tech <kai5266@gmail.com>
2021-02-25 13:54:46 +0800

    clean fragment

--- app/src/main/java/com/example/shipmentsystem/item/ItemFragment.kt
+++ app/src/main/java/com/example/shipmentsystem/item/ItemFragment.kt
@@ -73,7 +73,6 @@ class ItemFragment : Fragment() {
             clearEditText()
         }
 
-push test
         /** item delete */
         itemBinding.btnDelete.setOnClickListener {
             val selectedItem = itemViewModel.selectedItem.value
