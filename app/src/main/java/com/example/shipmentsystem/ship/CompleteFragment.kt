a34f0b92808d0e06710f78771b2ef20de66a6d3e
Kai <kai5266@gmail.com>
2021-03-24 22:37:58 +0800

    test action in CompleteFragment.kt

--- app/src/main/java/com/example/shipmentsystem/ship/CompleteFragment.kt
+++ app/src/main/java/com/example/shipmentsystem/ship/CompleteFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.shipmentsystem.R
+import com.example.shipmentsystem.databinding.FragmentCompleteBinding
+import com.example.shipmentsystem.databinding.FragmentProcessingBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +22,30 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CompleteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var processingBinding: FragmentCompleteBinding? = null
+    private val binding get() = processingBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete, container, false)
+        processingBinding = FragmentCompleteBinding.inflate(inflater,container,false)
+
+        binding.button.setOnClickListener {
+            getNavController().navigate(R.id.action_shipFragment_to_editFragment)
+        }
+
+     return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CompleteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CompleteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment = requireActivity().supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+
+    }//set NavHostFragment, a container for fragments
+
 }
