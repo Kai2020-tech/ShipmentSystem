597e32254227ec80b928908335b6a90402bc9f3e
Kai <kai5266@gmail.com>
2021-04-01 00:02:56 +0800

    dev date search,remove SearchVm.kt comment

--- app/src/main/java/com/example/shipmentsystem/ship/search/SearchVm.kt
+++ app/src/main/java/com/example/shipmentsystem/ship/search/SearchVm.kt
@@ -3,17 +3,12 @@ package com.example.shipmentsystem.ship.search
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.example.shipmentsystem.db.MyDatabase
 import com.example.shipmentsystem.db.ProcessingItem
-import kotlinx.coroutines.launch
 import java.util.*
 
 class SearchVm(application: Application) : AndroidViewModel(application) {
     private var dbDao = MyDatabase.getInstance(application).dao
-//    lateinit var startDate: Date
-//    lateinit var endDate: Date
 
     private lateinit var _searchList: LiveData<List<ProcessingItem>>
     val searchList: LiveData<List<ProcessingItem>>
@@ -21,8 +16,6 @@ class SearchVm(application: Application) : AndroidViewModel(application) {
 
 
     fun getSearchDate(start: Date, end: Date) {
-//        startDate = start
-//        endDate = end
         _searchList = dbDao.searchOrderList(start, end)
     }
 }
